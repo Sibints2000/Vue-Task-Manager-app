90dc61ef592ba15a690f627015471e067a50ab55
Sibints2000 <sibints2000@gmail.com>
2021-09-08 21:47:32 +0530

    Forty Second Commit

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-    devServer: {
-        
-    }
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: { '^/api': '/' },
+      },
+    },
+  },
 }
