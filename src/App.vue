8cc45e0ee0901d52010bcb186aca72a80bdc0f39
Sibints2000 <sibints2000@gmail.com>
2021-09-08 17:29:36 +0530

    Fourth Commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container">
-    <h1>Hello World</h1>
+    <Header />
   </div>
   
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    
-  }
+    Header
+  },
 }
 </script>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,18 @@
 </template>
 
 
-
-
-
 <script>
 export default {
     name: 'Header',
 }
 </script>
+
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
